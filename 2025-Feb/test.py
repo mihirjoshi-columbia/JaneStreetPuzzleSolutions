01c0086c7e9ac06fa1eedc0c1c7b744eafb13ef3
Mihir Joshi <mnj2122@columbia.edu>
2025-02-22 15:26:42 -0500

    So confused

--- 2025-Feb/test.py
+++ 2025-Feb/test.py
@@ -1,31 +1,60 @@
-w1 = "ADMIRERS"
-w2 = "ASTIR"
-w3 = "BLACK"
-w4 = "DITHER"
-w5 = "DRINK"
-w6 = "HOAGIES"
-w7 = "JOLTS"
-w8 = "OKRA"
-w9 = "PREMISE"
-w10 = "STICKERS"
-w11 ="SURFACED"
-w12 ="SWARM"
-w13 ="WILTS"
-w14 ="WRAP"
-w15 ="ARK"
-w16 ="CHILDRENS"
-w17 ="CUIRASS"
-w18 ="FOR"
-w19 ="HOE"
-w20 ="ISOMER"
-w21 ="LANE"
-w22 ="LORDS"
-w23 ="NOCTURNES"
-w24 ="RIDDLE"
-w25 ="SAT"
-w26 ="SOLE"
-w27 ="TRIONYM"
-w28 ="TROPE"
+# Ordering of words could be wrong
+rw1 = "ADMIRERS"
+rw2 = "ASTIR"
+rw3 = "BLACK"
+rw4 = "DITHER"
+rw5 = "DRINK"
+rw6 = "HOAGIES"
+rw7 = "JOLTS"
+rw8 = "OKRA"
+rw9 = "PREMISE"
+rw10 = "STICKERS"
+rw11 ="SURFACED"
+rw12 ="SWARM"
+rw13 ="WILTS"
+rw14 ="WRAP"
+bw1 = "ARK"
+bw2 ="CHILDRENS"
+bw3 ="CUIRASS"
+bw4 ="FOR"
+bw5 ="HOE"
+bw6 ="ISOMER"
+bw7 ="LANE"
+bw8 ="LORDS"
+bw9 ="NOCTURNES"
+bw10 ="RIDDLE"
+bw11 ="SAT"
+bw12 ="SOLE"
+bw13 ="TRIONYM"
+bw14 ="TROPE"
+
+'''
+1r on the 6r
+2b 4b
+5r 11r on the 11b 12b 
+The 10b 5b 3b 
+7b of the 13b 6b 
+... and the 3r 1b 
+4r 9r 
+13r 8r 
+10r 14b 
+2r 12r 
+8b of the 14r 
+7r of a 9b 
+'''
+
+sum = 0
+for i in range(len(rw1)):
+    print(ord(rw1[i]) - 64, rw1[i])
+    sum += ord(rw1[i]) - 64
+
+bsum = 0
+for i in range(len(bw1)):
+    print(ord(bw1[i]) - 64, bw1[i])
+    bsum += ord(bw1[i]) - 64
+
+print(sum, bsum)
+
 
 
 
